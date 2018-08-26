19710b750558911999843f353a74b67029800ec6
Seinnuhtwe97 <seinnuhtwe@gmail.com>
2018-08-26 20:53:04 +0630

    added

--- main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, url_for
-
-myapp = Flask(__name__)
-@myapp.route("/")
-def home():
-    return render_template("index.html")
-if __name__ == "__main__":
-    myapp.run(debug=True)
